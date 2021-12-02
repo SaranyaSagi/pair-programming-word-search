373c4be65bc3b06fec49e9c356926e3abba8ebed
SaranyaSagi <saranya108sagi@gmail.com>
2021-12-01 22:11:15 -0500

    fixed the bug to make tests pass and also improved the function so that both horizontal and vertical words are checked using previous row-column switch function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//Added a function that switched rows & colums and then checks so both horizontal and vertical are checked.
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  let switchLetters = letters[0].map((col, c) => letters.map((row, r) => letters[r][c]));
+  const horizontalJoin2 = switchLetters.map(ls => ls.join(''));
+  for (let l of horizontalJoin2) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
+
+
+//Original fixed code
+// const wordSearch = (letters, word) => {
+//     const horizontalJoin = letters.map(ls => ls.join(''))
+//     for (l of horizontalJoin) {
+//         if (l.includes(word)) return true
+//     }
+//     return false;
+// }
+
+// module.exports = wordSearch
+
